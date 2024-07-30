d99c2de8191aec1995ff28506567c1066a0d2872
otanikok <kavigon114@gmail.com>
2024-07-30 20:21:04 +0900

    リレーション

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     use HasFactory;
+    
+    public function songs()   
+{
+    return $this->hasMany(Song::class);  
+}
+    public function festivals(){
+        //フェスには複数のアーティストが出演している
+        return $this->belongsToMany(Festival::class);
+    }
+    public function comments()   
+{
+    return $this->hasMany(Comment::class);  
+}
+    
 }
